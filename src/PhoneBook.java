b224e86b0c18e550a5d8d1c2a227cf6fb415eef9
skalpel0410 <andreev_pa@list.ru>
2020-09-04 22:47:10 +0600

    Add method list

--- src/PhoneBook.java
+++ src/PhoneBook.java
@@ -1,6 +1,8 @@
 public class PhoneBook {
 
     public static void main(String[] args) {
+        String[][] book = new String[1000][2];
+
         //Добавить считывание ввода пользователя в цикле
     }
 
@@ -25,6 +27,8 @@ public class PhoneBook {
     }
 
     public static void list(String[][] book) {
-        //print phone book
+        for (int i = 0; i < book.length; i++) {
+            System.out.println(book[i][0].substring(book[i][1].indexOf(" ")+1,book[i][0].lastIndexOf(" ")-1) + ":" + book[i][1]);
+        }
     }
 }
